ad67bfd98d5ea7a48aafaeed48882170facd07e7
jbrucker <jebrucker@gmail.com>
2017-03-17 12:48:30 +0700

    add demo file

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * Run something from the store JAR file.
+ */
+public class Demo {
+
+	public static void main(String [] args) {
+		// run the main class from the JAR
+		store.Main.main();
+	}
+
+}
