bb17ecdc62ae49ac0ec400e160e1171150ad7128
jbrucker <jebrucker@gmail.com>
2017-03-17 12:54:52 +0700

    update demo code

--- src/Demo.java
+++ src/Demo.java
@@ -1,11 +1,12 @@
 /**
  * Run something from the store JAR file.
+ * It probably looks like problem 1 of the lab assignment.
  */
 public class Demo {
 
 	public static void main(String [] args) {
 		// run the main class from the JAR
-		store.Main.main();
+		store.Main.main( new String[]{} );
 	}
 
 }
